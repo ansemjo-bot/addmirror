1ae2d5f499397f6241f76dce026fb279abcc1393
Anton Semjonov <anton@semjonov.de>
2016-06-14 22:10:01 +0200

    check local bare

--- addmirror.sh
+++ addmirror.sh
@@ -3,8 +3,17 @@
 # fail entire script on any error
 set -e
 
+
+    remote="$1"
+    bare="$2"
+
+    hooks_dir="custom_hooks"
+    hashbin="/usr/bin/sha256sum"
+
+
 # print some info with bold '>>>' prefixed
 function info { echo -e "\e[1m>>>\e[0m $@"; }
+
 
 # check if remote repo exists and is accessible
 function check_remote {
@@ -12,4 +21,15 @@ function check_remote {
     git ls-remote --quiet "$1" && echo "OK."
 }
 
-check_remote "$1"
+function check_bare {
+    info "Checking local bare repository ..."
+    if test "$(git -C "$1" rev-parse --is-bare-repository)" == "true"; then
+        echo "OK."
+    else
+        echo "Repository '$1' does not exist or is not a bare repository."
+        return 1
+    fi
+}
+
+check_bare "$bare"
+check_remote "$remote"
