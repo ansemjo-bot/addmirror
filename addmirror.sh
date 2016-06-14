5193e68db83a25be2dd849c3d0da9123444542d5
Anton Semjonov <anton@semjonov.de>
2016-06-14 23:36:07 +0200

    better commenting

--- addmirror.sh
+++ addmirror.sh
@@ -12,16 +12,17 @@ set -e
     gitbin="git"
     hashbin="md5sum"
 
-    blogpost="https://smcleod.net/mirror-gitlab-to-github/"
-    gitlab_customhooks="http://docs.gitlab.com/ce/hooks/custom_hooks.html"
-    github_machineuser="https://developer.github.com/guides/managing-deploy-keys/#machine-users"
+    # MORE INFO
+    blogpost="https://smcleod.net/mirror-gitlab-to-github/" # original blogpost
+    gitlab_customhooks="http://docs.gitlab.com/ce/hooks/custom_hooks.html" # why 'custom_hooks'?
+    github_machineuser="https://developer.github.com/guides/managing-deploy-keys/#machine-users" # add machine user as collaborator on GitHub
 
 
-# print some info with bold '>>>' prefixed
+# function to print some info in bold with '>>>' prefixed
 function info { echo -e "\e[1m>>> $@ ...\e[0m"; }
 
 
-# check if running as correct user
+#### CHECK EFFECTIVE UID ####
 if ! test "$EUID" == "$(id -u git)"; then
     info "You are not running as user '$gituser'!";
     read -n 1 -p "Are you sure you want to continue? [y/n] " sure
@@ -33,25 +34,25 @@ if ! test "$EUID" == "$(id -u git)"; then
 fi
 
 
-# check if local repository exists and is bare
+#### CHECK LOCAL REPOSITORY ####
 info "Checking existence of local bare repository"
 if ! test "$($gitbin -C "$bare" rev-parse --is-bare-repository)" == "true"; then
     echo "Repository '$bare' does not exist or is not a bare repository."; false;
 fi
 
 
-# check if remote repo exists and is accessible
+#### CHECK REMOTE REPOSITORY ####
 info "Checking existence and access to remote repository"
 "$gitbin" ls-remote --quiet "$remote"
 
 
-# hash the remote url
+#### HASH REMOTE URL ####
 info "Hashing remote url"
 hashed="$(printf "%s" "$remote" | "$hashbin" | awk '{print $1}')"
 echo "using '$hashed' as hash"
 
 
-# add remote to local repository
+#### ADD REMOTE URL AS MIRRORING REMOTE ####
 info "Adding remote to local repository"
 if ! "$gitbin" -C "$bare" remote -v | grep "$hashed.*push"; then
     "$gitbin" -C "$bare" remote add --mirror=push "$hashed" "$remote"
@@ -60,7 +61,7 @@ else
 fi
 
 
-# add post-receive hook
+#### CREATE POST-RECEIVE HOOK ####
 info "Adding 'post-receive' hook"
 mkdir -p "$bare/$hooks_dir"
 hook="$bare/$hooks_dir/post-receive"
@@ -76,8 +77,3 @@ if ! grep "$hash" "$hook"; then
 else
     echo "a hook for this hash already exists in '$hook'!"
 fi
-
-
-
-
-
