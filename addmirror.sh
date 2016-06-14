f88cd7f8bb0eadfcd0cb56711f5be45144a068a1
Anton Semjonov <anton@semjonov.de>
2016-06-14 21:52:02 +0200

    check remote repo

--- addmirror.sh
+++ addmirror.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-function ssh_check {
-    echo "sshcheck ...";
+# fail entire script on any error
+set -e
+
+# print some info with bold '>>>' prefixed
+function info { echo -e "\e[1m>>>\e[0m $@"; }
+
+# check if remote repo exists and is accessible
+function check_remote {
+    info "Checking remote repository ..."
+    git ls-remote --quiet "$1" && echo "OK."
 }
 
-ssh_check
+check_remote "$1"
