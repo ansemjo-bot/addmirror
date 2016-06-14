6ebb438900f3c73b42cb61d1d13f6f111e59964e
Anton Semjonov <anton@semjonov.de>
2016-06-14 23:30:29 +0200

    check username

--- addmirror.sh
+++ addmirror.sh
@@ -6,21 +6,37 @@ set -e
 
     bare="$1"
     remote="$2"
-    hashed=""
 
-    hooks_dir="custom_hooks"
-    gitbin="/usr/bin/git"
-    hashbin="/usr/bin/md5sum"
+    hooks_dir="custom_hooks" # --> see $gitlab_customhooks
+    gituser="git"
+    gitbin="git"
+    hashbin="md5sum"
+
+    blogpost="https://smcleod.net/mirror-gitlab-to-github/"
+    gitlab_customhooks="http://docs.gitlab.com/ce/hooks/custom_hooks.html"
+    github_machineuser="https://developer.github.com/guides/managing-deploy-keys/#machine-users"
 
 
 # print some info with bold '>>>' prefixed
 function info { echo -e "\e[1m>>> $@ ...\e[0m"; }
 
 
+# check if running as correct user
+if ! test "$EUID" == "$(id -u git)"; then
+    info "You are not running as user '$gituser'!";
+    read -n 1 -p "Are you sure you want to continue? [y/n] " sure
+    if test "$sure" == "y"; then
+        echo "es" #yes
+    else
+        echo; false;
+    fi
+fi
+
+
 # check if local repository exists and is bare
 info "Checking existence of local bare repository"
 if ! test "$($gitbin -C "$bare" rev-parse --is-bare-repository)" == "true"; then
-    echo "Repository '$bare' does not exist or is not a bare repository."; return 1
+    echo "Repository '$bare' does not exist or is not a bare repository."; false;
 fi
 
 
